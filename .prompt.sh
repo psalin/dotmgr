0482a6a93ba96e7227b3dc7180c3af9ab80a7477
Javier San Jose <javiersanjoseglez@gmail.com>
2019-01-06 14:15:57 +0200

    bash prompt: add support for different hostname/color
        - Now is easy to change color/hostname using the function
            "set_prompt hostname color"

--- .prompt.sh
+++ .prompt.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # Show git brach
-function parse_git_branch
-{
+function parse_git_branch() {
     git branch --no-color 2>/dev/null | grep \* | sed "s/\* \(.*\)/ (\1)/"
 }
 
-# If git is not installed, the prompt will have a little bit of delay, because
-# of parse_git_branch function
-export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)\$ '
+function set_prompt() {
+    local hostname=${1:-"\\h"}
+    local prompt_color=${2:-"32"}
+
+    # If git is not installed, the prompt will have a little bit of delay, because
+    # of parse_git_branch function
+    export PS1="\[\033[01;${prompt_color}m\]\u@${hostname}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)\$ "
+}
+
+
+set_prompt
