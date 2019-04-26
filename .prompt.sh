bdab750a325f4f1cc5bbb8fc849393d824730a47
Javier San Jose <javiersanjoseglez@gmail.com>
2019-04-27 00:16:30 +0300

    prompt: don't show git branch name if git is not installed

--- .prompt.sh
+++ .prompt.sh
@@ -2,16 +2,18 @@
 
 # Show git brach
 function parse_git_branch() {
-    git branch --no-color 2>/dev/null | grep \* | sed "s/\* \(.*\)/ (\1)/"
+    git branch --no-color 2>/dev/null | grep -E "^.*" | sed "s/\* \(.*\)/ (\1)/"
 }
 
 function set_prompt() {
     local hostname=${1:-"\\h"}
     local prompt_color=${2:-"32"}
 
-    # If git is not installed, the prompt will have a little bit of delay, because
-    # of parse_git_branch function
-    export PS1="\[\033[01;${prompt_color}m\]\u@${hostname}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$(parse_git_branch)\$ "
+    if ! dpkg -l | grep -E "^ii\s\svim\s+" &> /dev/null; then
+        export PS1="\[\033[01;${prompt_color}m\]\u@${hostname}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+    else
+        export PS1="\[\033[01;${prompt_color}m\]\u@${hostname}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$(parse_git_branch)\$ "
+    fi
 }
 
 
