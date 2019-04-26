b842e525096ee54a76ada68e8829c4e52bb886e7
Javier San Jose <javiersanjoseglez@gmail.com>
2019-04-27 00:19:08 +0300

    prompt: fix wrong grep for git package

--- .prompt.sh
+++ .prompt.sh
@@ -9,7 +9,7 @@ function set_prompt() {
     local hostname=${1:-"\\h"}
     local prompt_color=${2:-"32"}
 
-    if ! dpkg -l | grep -E "^ii\s\svim\s+" &> /dev/null; then
+    if ! dpkg -l | grep -E "^ii\s\sgit\s+" &> /dev/null; then
         export PS1="\[\033[01;${prompt_color}m\]\u@${hostname}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
     else
         export PS1="\[\033[01;${prompt_color}m\]\u@${hostname}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$(parse_git_branch)\$ "
