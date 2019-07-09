5f65638a8513393158fcb24045cea08955446f42
Peter Salin <esalipe@users.noreply.github.com>
2019-07-09 07:39:10 +0000

    move vim installation to script
    Moves vim installation from install.sh to its own script.

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ set -euo pipefail
 # TODO:
 #   - dry run option
 
-# This script assumes the use of vim-plug as vim plugin manager, with default
-# paths for files i.e. ~/.vim/plugged/
-
 dotfiles_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 script_dir=scripts
 script_help_cmd="${script_dir}/help.sh"
@@ -35,7 +32,6 @@ parameter_dotfiles=false
 parameter_basic_packages=false
 parameter_packages=false
 parameter_scripts=()
-parameter_vim=false
 parameter_xfce_terminal=false
 parameter_gnome_terminal=false
 
@@ -234,56 +230,6 @@ function install_packages() {
     fi
 }
 
-function install_vim() {
-    local packages_not_installed=()
-    for package in "${basic_packages_list[@]}"; do
-        if ! check_package "${package}"; then
-            __log_info "${package}: Not installed"
-            packages_not_installed+=("${package}")
-            continue
-        fi
-    done
-
-    if [ ${#packages_not_installed[@]} -ne 0 ]; then
-        __log_warning "VIM 8: Basic packages are not installed. Skipping..."
-        __log_warning "       Please use --basic-packages in addition to --vim"
-        return
-    fi
-
-    if ! vim --version | head -n1 | grep -E "^.*IMproved 8.*" &> /dev/null; then
-        __log_info "VIM 8: Not installed"
-        echo
-        echo "Downloading and compiling VIM 8"
-        build_directory="${HOME}/buildvim8"
-        git clone https://github.com/vim/vim "${build_directory}"
-        cd "${build_directory}"
-        ./configure --prefix="${HOME}/.local/"
-        make install
-    fi
-    __log_success "VIM 8: Installed"
-
-    if [ ! -f "${HOME}/.vim/autoload/plug.vim" ]; then
-        __log_info "vim-plug: Not installed"
-        echo
-        echo "Downloading vim-plug"
-        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    fi
-    __log_success "vim-plug: Installed"
-
-    vim -c 'PlugUpdate' -c 'q!' -c 'q!'
-    __log_success "vim-plug: Plugins updated"
-
-    # If we have YouCompleteMe plugin in vim, the plugin will be installed
-    # automatically, but we still need to compile the core
-    if grep -Eq "^\s*Plug .*YouCompleteMe.*$" "${HOME}/.vimrc.plugins"; then
-        "${HOME}"/.vim/plugged/YouCompleteMe/install.py
-        __log_success "YouCompleteMe: Plugin installed and core compiled"
-    else
-        __log_info "YouCompleteMe: plugin not activated in the configuration. Skipping..."
-    fi
-}
-
 function read_conffile() {
     local conffile="$1"
 
@@ -364,11 +310,6 @@ function install_main() {
         install_dotfiles "${xfce_profile[@]}"
     fi
 
-    if [ "${parameter_vim}" = true ]; then
-        echo
-        echo "Installing VIM 8 and plugins"
-        install_vim
-    fi
     echo
 }
 
@@ -382,7 +323,6 @@ If no argument is indicated, the script will not perform any action.
 List of arguments:
   --conffile conffile       Path to the configuration file to use
   --dotfiles                Install the dotfiles
-  --vim                     Install VIM 8 and VIM plugins
   --basic-packages          Install basic packages:
                                 - curl
                                 - git
@@ -418,10 +358,6 @@ while (( "$#" )); do
             ;;
         --dotfiles)
             parameter_dotfiles=true
-            shift
-            ;;
-        --vim)
-            parameter_vim=true
             shift
             ;;
         --basic-packages)
